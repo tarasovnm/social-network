4f384db3e265e9d73c3474bf14e53f672277d812
tarasovnm <tarasovnm@gmail.com>
2020-11-23 01:33:17 +0400

    Done base markup for dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,24 @@ import s from './Dialogs.module.scss';
 function Dialogs() {
     return (
         <div className={s.dialogs}>
-            Dialogs
+            <h3 className={s.dialogs__title}>Dialogs</h3>
+
+            <div className={s.dialogs__wrapper}>
+                <ul className={s.dialogs__list}>
+                    <li className={s.dialog}>Dimych</li>
+                    <li className={s.dialog + ' '+ s.active}>Andrey</li>
+                    <li className={s.dialog}>Sveta</li>
+                    <li className={s.dialog}>Alex</li>
+                    <li className={s.dialog}>Victor</li>
+                    <li className={s.dialog}>Valera</li>
+                </ul>
+
+                <ul className={s.dialogs__messages}>
+                    <li className={s.message}>Hi</li>
+                    <li className={s.message}>Hi! How are You?</li>
+                    <li className={s.message}>Fine! Thanks!</li>
+                </ul>
+            </div>
         </div>
     );
 }
