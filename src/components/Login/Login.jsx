f5bc31ae97f8fabee4d8e2aa7d311bd7fe182dfd
tarasovnm <tarasovnm@gmail.com>
2021-01-13 12:17:44 +0400

    Added redux-form package and login form.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,39 @@
-const Login = (Login) => {
+import s from './Login.module.scss';
+import {Field, reduxForm} from 'redux-form'
+
+const LoginForm = (props) => {
   return (
-    <h2>Login page</h2>
+    <form className={s.form} onSubmit={props.handleSubmit}>
+      <div className={s.form__item}>
+        <Field className={s.form__input} component="input" type="text" name="login" placeholder="login" />
+      </div>
+      <div className={s.form__item}>
+        <Field className={s.form__input} component="input" type="text" name="password" placeholder="password" />
+      </div>
+      <div className={s.form__item}>
+        <label>
+          <Field className={s.form__input} component="input" type="checkbox" name="rememberMe" /> remember me
+          </label>
+      </div>
+      <div className={s.form__item}>
+        <button className={s.form__btn}>Sign in</button>
+      </div>
+    </form>
+  );
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
+
+const Login = (props) => {
+  const onSubmit = (formData) => {
+    console.log(formData)
+  }
+
+  return (
+    <div className={s.login}>
+      <h3 className={s.login__title}>Login page</h3>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </div>
   );
 }
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,5 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import {reducer as formReducer} from 'redux-form'
 import thunkMiddleware from "redux-thunk";
 import profileReducer from "./profileReducer";
 import messagesReducer from "./messagesReducer";
@@ -6,12 +7,14 @@ import sidebarReducer from "./sidebarReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 
+
 let reduceds = combineReducers({
   profilePage: profileReducer,
   messagesPage: messagesReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
 
 let store = createStore(reduceds, applyMiddleware(thunkMiddleware));
